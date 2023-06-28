8ad24603792d5455376e3f64ef7b80418c86e47b
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2023-06-28 11:45:17 -0700

    amend: first cut

--- test/adhoc/test1.chezscheme.scm
+++ test/adhoc/test1.chezscheme.scm
@@ -4,4 +4,4 @@
  '((relfPythonipc . "../datriples/dagopoly/test/autobin/testipc1.py")))
 (define b1 (block-python "expr3"))
 (display (format "sig=~a\n" (block-sig b1)))
-(display (format "sig=~a\n" (s->list (block-get b1))))
+(display (format "get=~a\n" (s->list (block-get b1))))
