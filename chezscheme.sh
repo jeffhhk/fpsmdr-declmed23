5f49b249254a6163580e84a02528da34b2a21e11
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2023-07-07 13:09:43 -0700

    fix: akku and chez libdirs integration issues

--- chezscheme.sh
+++ chezscheme.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 adirProj=$( cd $( dirname "$0" ) && pwd )
-# Set CHEZSCHEMELIBDIRS, etc via akka
-. "$adirProj/.akku/bin/activate"
+cd "$adirProj"                       # Work around an cwd dependency bug in akku
+. "$adirProj/.akku/bin/activate"     # Set CHEZSCHEMELIBDIRS, etc via akku
 # When Chez is started as a repl, (command-line) evaluates to (""), because there
 # is no "current script."  Thus to find our default storage/ directory, we record
 # our $0 so that it can be retrieved by Chez even when started as a repl.
 export adirProj
-# Run Chez
 # In addition to the akku directories, the project root must be in the libdirs.
-exec scheme --libdirs "$adirProj:$CHEZSCHEMELIBDIRS" "$@"
+export CHEZSCHEMELIBDIRS="$adirProj:$CHEZSCHEMELIBDIRS"
+# Run Chez
+exec scheme "$@"
