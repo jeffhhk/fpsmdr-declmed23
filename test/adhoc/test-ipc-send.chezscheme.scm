7e9fe6ed7db34975dc0544ab57e6ecadb83af469
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2023-07-14 13:00:46 -0700

    fix: format ipc demo with ~s not ~a

--- test/adhoc/test-ipc-send.chezscheme.scm
+++ test/adhoc/test-ipc-send.chezscheme.scm
@@ -5,5 +5,5 @@
 (current-io
  '((relfPythonipc . "../dagopoly-py/test/autobin/test-ipc-receive.py")))
 (define b1 (block-python "expr3"))
-(display (format "sig=~a\n" (block-sig b1)))
-(display (format "get=~a\n" (s->list (block-get b1))))
+(display (format "sig=~s\n" (block-sig b1)))
+(display (format "get=~s\n" (s->list (block-get b1))))
